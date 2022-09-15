bb04073a89a4aa475ae315383af8441926ed7c43
umair.abid <umair.abid96@gmail.com>
2022-09-15 18:49:12 +0500

    Complete Catalog Service

---
+++ src/Services/Catelog/Catelog.API/Extensons/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Repositories;
+
+namespace Catelog.API.Extensons
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection ServiceConfig(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICatalogContext, CatalogContext>();
+
+            return services;
+        }
+    }
+}
--- src/Services/Catelog/Catelog.API/Program.cs
+++ src/Services/Catelog/Catelog.API/Program.cs
@@ -1,3 +1,5 @@
+using Catelog.API.Extensons;
+
 namespace Catelog.API
 {
     public class Program
@@ -7,6 +9,8 @@ namespace Catelog.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.ServiceConfig();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
